0d4513f481c0d767af737dabb604c938dba95890
samChen69 <cpan520@qq.com>
2017-10-12 16:31:31 +0800

    update react-study

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
 	entry:'./src/main.js',
@@ -6,25 +8,39 @@ module.exports = {
 		path:__dirname+'dist',
 		filename:'[name].bundle.js'
 	},
-	loader:[
-		{
-			test:/.js$/,
-			loader:'babel-loader'
-		},
-		{
-			test:/.css$/,
-			loader:'style-loader!css-loader'
-		},
-		{
-			test:/.(png|jpg|gif)$/,
-			loader:'file-loader'
-		},
-		{
-			test:/.(woff|woff2|eot|ttf|otf)$/,
-			use:['file-loader']
-		}
-	],
+	module:{
+		loaders:[
+			{
+				test:/.js$/,
+				exclude:path.resolve(__dirname,'node_modules'),
+				loader:'babel'
+			},
+			{
+				test:/.jsx$/,
+				exclude:path.resolve(__dirname,'node_modules'),
+				loader:'babel'
+			},
+			{
+				test:/.css$/,
+				loader:'style-loader!css-loader'
+			},
+			{
+				test:/.(png|jpg|gif)$/,
+				loader:'file-loader'
+			},
+			{
+				test:/.(woff|woff2|eot|ttf|otf)$/,
+				use:['file-loader']
+			}
+		],
+	},
 	plugins:[
-
+		new htmlWebpackPlugin({
+			filename:'index.html',
+			template:'index.html',
+			inject:'body'
+		}),
+		new webpack.HotModuleReplacementPlugin()
 	],
+	devtool:'inline-source-map'
 }
