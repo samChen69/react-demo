6b0b6350600508e640f616650b867d4223b87f4a
samChen69 <cpan520@qq.com>
2017-10-13 20:49:19 +0800

    study react

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,25 @@
 import React from 'react';
 class Form extends React.Component{
 	constructor(props){
-		super(props)
+		super(props);
+		this.state = {
+			name:'',
+			pwd:''
+		}
+		this.handleClick = this.handleClick.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(event){
+		let tgt = event.target;
+		let k = tgt.name;
+		let v = tgt.value;
+		this.setState({
+			[k]:v
+		})
+	}
+	handleClick(event){
+		event.preventDefault();
+		console.log(`name:${this.state.name},pwd:${this.state.pwd}`)
 	}
 	render(){
 
@@ -10,12 +28,13 @@ class Form extends React.Component{
 			<div>
 				<h1>Form example</h1>
 				<form>
-					<div><label>Name:</label><input type="text" name="name" /></div>
-					<div><label>Passwords:</label><input type="text" name="pwd" /></div>
+					<div><label>Name:</label><input onChange={this.handleChange} value={this.state.name} type="text" name="name" /></div>
+					<div><label>Passwords:</label><input onChange={this.handleChange} value={this.state.pwd} type="password" name="pwd" /></div>
+					<div><button onClick={this.handleClick}>submit</button></div>
 				</form>
 			</div>
 		)
 	}
 }
 
-export default Form
+export default Form;
--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import ReactDOM from 'react-dom'
 // import Clock from './components/Clock.js';
 import List from './components/List.js'
 import Form from './components/Form.js'
-
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
 	[
 		<List numbers={[1,2,3,4,5,6]} key="0" />,
-		<Form />
+		<Form key="1" />,
+		<div key="2">{false}</div>,
+		<Button type="primary">Primary</Button>,
+		<Button type="primary" shape="circle" icon="search" />
 	],
 	document.getElementById('app')
 )
